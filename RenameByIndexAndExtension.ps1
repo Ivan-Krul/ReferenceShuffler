7df3dd090879e5819c4a5cdcb4bf05dfc3445ec5
Ivan-Krul <kiwiivanya128@gmail.com>
2024-01-21 20:24:07 +0100

    made powershell file for better renaming and did it in test chamber

--- RenameByIndexAndExtension.ps1
+++ RenameByIndexAndExtension.ps1
@@ -1,38 +1,129 @@
 $items = Get-ChildItem
     
-$uniqueExtension = @()
-$countExtension =  @()
+function Extract-StringFromFilename {
+    param (
+        [string]$filename,
+        [string]$pattern
+    )
 
-$isExist = 0
-$listAuthNames = @()
-$listFutuNames = @()
-
-foreach($item in $items) {
-    $isExist = -1
-
-    for ($i = 0; $i -lt $uniqueExtension.Count; $i++) {
-        if($uniqueExtension[$i] -eq $item.Extension) {
-            $isExist = $i
-            $countExtension[$i] += 1
-            break
-        }
+    if ($filename -match $pattern) {
+        return $matches[0]
+    } else {
+        return $null
     }
+}
 
-    if($isExist -eq -1) {
-        if($item.Extension -eq ".ps1") {
+function Get-UniqueAuthors {
+    param(
+        [System.IO.FileInfo[]] $list
+    )
+
+    $uniqueList = @()
+
+    foreach($item in $list) {
+        $resStr = Extract-StringFromFilename -filename $item.Name -pattern "(((u|t|(4c))-)|@)[a-zA-Z0-9_-]+"
+
+        if($resStr -eq "") {
             continue
         }
-        
-        $uniqueExtension += $item.Extension
-        $countExtension += 0
-        $isExist = $uniqueExtension.Length - 1
-    }
-    $listAuthNames += $item.Name.Split(" ")[0]
-    $listFutuNames += "$($countExtension[$isExist])$($uniqueExtension[$isExist])"
-    Write-Host $item.Name
-    Write-Host "$($countExtension[$isExist])$($uniqueExtension[$isExist])"
-    #Rename-Item $item "$($countExtension[$isExist])$($uniqueExtension[$isExist])"
-}
-Write-Host $listAuthNames
-Write-Host $listFutuNames
 
+        $isNotExist = 1
+
+        foreach($writed in $uniqueList) {
+            if($writed -eq $resStr) {
+                $isNotExist = 0
+                break
+            }
+        }
+
+        if($isNotExist -eq 1) {
+            $uniqueList += $resStr
+        }
+    }
+
+    return $uniqueList
+}
+
+function Count-Uniques {
+    param (
+        [String[]] $items,
+        [String[]] $listUniques
+    )
+
+    $counter = New-Object int[] $listUniques.Count
+    for($i = 0; $i -lt $listUniques.Count; $i += 1) {
+        $counter[$i] = 0
+    }
+
+    foreach($item in $items){
+        $resStr = Extract-StringFromFilename -filename $item -pattern "(((u|t|(4c))-)|@)[a-zA-Z0-9_-]+"
+
+        if(-not $resStr) {
+            continue
+        }
+
+        for($i = 0; $i -lt $listUniques.Count; $i += 1) {
+            
+            if($listUniques[$i] -eq $resStr) {
+                $counter[$i] += 1
+                break
+            }
+        }
+    }
+
+    return $counter
+}
+
+function Move-ToRelatedFolders {
+    param (
+        [System.IO.FileInfo[]] $list
+    )
+
+    foreach($item in $list) {
+        $resStr = Extract-StringFromFilename -filename $item.Name -pattern "(((u|t|(4c))-)|@)[a-zA-Z0-9_-]+"
+
+        if($resStr -eq "") {
+            continue
+        }
+
+        Write-Host $resStr
+
+        Move-Item -LiteralPath $item -Destination $resStr
+    }
+}
+
+function Rename-SubFolderGuts {
+    param (
+        [String[]] $listUniq,
+        [int[]] $counts
+    )
+
+    $currentLocation = Get-Location
+
+    for($i = 0; $i -lt $listUniq.Count; $i += 1) {
+        Set-Location $listUniq[$i]
+        $localItems = Get-ChildItem -File
+        Write-Host "$($listUniq[$i]) Current Location: $(Get-Location)"
+        
+        foreach($item in $localItems) {
+            $extension = $item.Extension
+            Rename-Item -LiteralPath $item.FullName -NewName "$($counts[$i])$($extension)"
+            $counts[$i] -= 1
+        }
+
+        Set-Location $currentLocation
+    }
+}
+
+$list = Get-ChildItem -File
+$uniqNames = Get-UniqueAuthors -list $list
+
+foreach($name in $uniqNames) {
+    New-Item -ItemType Directory -Name $name
+}
+
+
+$counts = Count-Uniques -items $list -listUniques $uniqNames
+
+Move-ToRelatedFolders -list $list
+Rename-SubFolderGuts -listUniq $uniqNames -counts $counts
